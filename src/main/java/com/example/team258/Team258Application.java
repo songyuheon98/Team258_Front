f477f3c2f64c8acb951c9c438db9eda9cb002ae3
songyuheon98 <songyuheon2750@gmail.com>
2023-10-04 20:28:29 +0900

    Team258

---
+++ src/main/java/com/example/team258/Team258Application.java
@@ -0,0 +1,13 @@
+package com.example.team258;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team258Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team258Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/team258/Team258ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team258;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team258ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
