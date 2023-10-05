dad0a5d2dad4e944086b6d21a32642a2d09ac1b4
yame123 <inyasang@gmail.com>
2023-10-05 19:39:27 +0900

    refactor : 출력 시의 데이터타입에 ResponseEntity와 MessageDto를 사용하도록 수정

--- src/main/java/com/example/team258/controller/AnswerController.java
+++ src/main/java/com/example/team258/controller/AnswerController.java
@@ -2,8 +2,11 @@ package com.example.team258.controller;
 
 import com.example.team258.dto.AnswerRequestDto;
 import com.example.team258.dto.AnswerResponseDto;
+import com.example.team258.entity.MessageDto;
 import com.example.team258.service.AnswerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -18,26 +21,22 @@ public class AnswerController {
     private final AnswerService answerService;
 
     @PostMapping("/answer")
-    public ResponseEntity<String> createAnswer(@RequestBody AnswerRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails){
-        ResponseEntity<String> answer = answerService.createAnswer(requestDto,userDetails.getUser());
-        return answer;
+    public ResponseEntity<MessageDto> createAnswer(@RequestBody AnswerRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        return ResponseEntity.ok(answerService.createAnswer(requestDto, userDetails.getUser()));
     }
 
     @GetMapping("/answer")
-    public List<AnswerResponseDto> getAnswers(@AuthenticationPrincipal UserDetailsImpl userDetails){
-        List<AnswerResponseDto> answer = answerService.getAnswers(userDetails.getUser());
-        return answer;
+    public ResponseEntity<List<AnswerResponseDto>> getAnswers(@AuthenticationPrincipal UserDetailsImpl userDetails){
+        return ResponseEntity.ok(answerService.getAnswers(userDetails.getUser()));
     }
 
     @PutMapping("/answer/{answerId}")
-    public ResponseEntity<String> updateAnswer(@RequestBody AnswerRequestDto requestDto,@PathVariable Long answerId, @AuthenticationPrincipal UserDetailsImpl userDetails){
-        ResponseEntity<String> answer = answerService.updateAnswer(requestDto, answerId,userDetails.getUser());
-        return answer;
+    public ResponseEntity<MessageDto> updateAnswer(@RequestBody AnswerRequestDto requestDto,@PathVariable Long answerId, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        return ResponseEntity.ok(answerService.updateAnswer(requestDto,answerId, userDetails.getUser()));
     }
 
     @DeleteMapping("/answer/{answerId}")
-    public ResponseEntity<String> deleteAnswer(@PathVariable Long answerId, @AuthenticationPrincipal UserDetailsImpl userDetails){
-        ResponseEntity<String> answer = answerService.deleteAnswer(answerId,userDetails.getUser());
-        return answer;
+    public ResponseEntity<MessageDto> deleteAnswer(@PathVariable Long answerId, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        return ResponseEntity.ok(answerService.deleteAnswer(answerId,userDetails.getUser()));
     }
 }
