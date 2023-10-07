4046c7fb4ccc1eab643f50b28772722ee59f68d6
INYONG KIM <ohnyong87@gmail.com>
2023-10-07 19:38:51 +0900

    feat : mypage.html 레이아웃 추가

---
+++ src/main/java/com/example/team258/controller/viewController/MyPageViewController.java
@@ -0,0 +1,12 @@
+package com.example.team258.controller.viewController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyPageViewController {
+    @GetMapping("/mypage")
+    public String login() {
+        return "mypage";
+    }
+}
