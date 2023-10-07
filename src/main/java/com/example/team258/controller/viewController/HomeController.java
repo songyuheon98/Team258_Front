303769fe8811fe42f1392d107e1624b7ccbd03e8
INYONG KIM <ohnyong87@gmail.com>
2023-10-07 14:21:47 +0900

    feat : Frontend - index.html 전반 작업, 레이아웃픽스, 반응형웹템플릿화, css,js 모듈화

---
+++ src/main/java/com/example/team258/controller/viewController/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.team258.controller.viewController;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
