39d954ece3a8015849ccbf2d886075d056c89fd0
songyuheon98 <songyuheon2750@gmail.com>
2023-10-09 16:08:17 +0900

    feat : 프론트작업 login & signup & escape

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('#login-btn').on('click', function() {
+    $('#login-btn').on('click', function () {
         const data = {
             username: $('#username').val(),
             password: $('#password').val()
@@ -14,11 +14,11 @@ $(document).ready(function() {
             data: JSON.stringify(data),
             contentType: 'application/json;charset=UTF-8',
             dataType: 'json',
-            success: function(response) {
+            success: function (response) {
                 alert('로그인 성공!');
                 window.location.href = "/";
             },
-            error: function(xhr, status, error) {
+            error: function (xhr, status, error) {
                 if (xhr.status === 401) {
                     alert('아이디 또는 비밀번호가 잘못되었습니다.');
                 } else {
@@ -26,16 +26,15 @@ $(document).ready(function() {
                 }
                 window.location.href = "/login";
             },
-            complete: function() {
+            complete: function () {
                 /**
                  * 요청이 완료된 후 로그인 버튼을 다시 활성화
-                  */
+                 */
                 $('#login-btn').removeAttr('disabled');
             }
         });
     });
+    $('#moveSignUpView-btn').on('click', function () {
+        window.location.href = '/signup';
+    });
 });
-
-function moveSignUpView(){
-    window.location.href = '/signup';
-}
