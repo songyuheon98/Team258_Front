30b1c397eeff068817f5baa1ecddf4ab6d1e0399
songyuheon98 <songyuheon2750@gmail.com>
2023-10-09 15:08:42 +0900

    Status : 프론트작업 login 완료

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,28 +1,40 @@
-// ajax 요청 샘플입니다.
-/*
 $(document).ready(function() {
-    $("#deleteColumnForm").on("submit", function(e) {
-        e.preventDefault();
+    $('#login-btn').on('click', function() {
+        const data = {
+            username: $('#username').val(),
+            password: $('#password').val()
+        };
 
-        const columnId = $("#columnId").val();
+        // 로그인 버튼 비활성화 (중복 요청 방지)
+        $(this).attr('disabled', 'disabled');
 
         $.ajax({
-            type: "DELETE",
-            url: "/api/column/" + columnId,
-            success: function() {
-                alert("컬럼 삭제에 성공하였습니다."); // 예상 응답에 따라 수정이 필요합니다.
-                window.history.back();
+            type: 'POST',
+            url: '/api/user/login',
+            data: JSON.stringify(data),
+            contentType: 'application/json;charset=UTF-8',
+            dataType: 'json',
+            success: function(response) {
+                alert('로그인 성공!');
+                window.location.href = "/";
             },
-            error: function(error) {
-                alert("컬럼 삭제에 실패하였습니다.");
+            error: function(xhr, status, error) {
+                if (xhr.status === 401) {
+                    alert('아이디 또는 비밀번호가 잘못되었습니다.');
+                } else {
+                    alert('로그인 실패!');
+                }
+                window.location.href = "/login";
+            },
+            complete: function() {
+                /**
+                 * 요청이 완료된 후 로그인 버튼을 다시 활성화
+                  */
+                $('#login-btn').removeAttr('disabled');
             }
         });
     });
 });
-*/
-function login(){
-    alert("login기능을 구현해 주세요.")
-}
 
 function moveSignUpView(){
     window.location.href = '/signup';
