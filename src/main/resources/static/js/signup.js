f0e4bfe09be4c0e6e1b4244bdcd402a024cf0d71
songyuheon98 <songyuheon2750@gmail.com>
2023-10-09 15:14:54 +0900

    feat : 프론트작업 login & signup 완료

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -1,3 +1,26 @@
-function signUp(){
-    alert("signup기능을 구현해 주세요.")
-}
+$(document).ready(function() {
+    $('#signup-button').on('click', function() {
+        var form = $('#signup-form');
+        var data = {
+            username: form.find('input[name="username"]').val(),
+            password1: form.find('input[name="password1"]').val(),
+            password2: form.find('input[name="password2"]').val()
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/user/signup',
+            data: JSON.stringify(data),
+            contentType: 'application/json;charset=UTF-8',
+            dataType: 'json',
+            success: function(response) {
+                alert('회원가입 성공!');
+                window.location.href = "/login";
+            },
+            error: function(xhr, status, error) {
+                alert('회원가입 실패!'); // 오류 메시지는 실제 API 응답에 따라 조절할 수 있습니다.
+                window.location.href = "/signup";
+            }
+        });
+    });
+});
