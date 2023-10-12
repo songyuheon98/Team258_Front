ee23f8e452470e534e0f0475f68b249f384ebf3d
DESKTOP-HMN2L1J\정강용 <tuto3355@naver.com>
2023-10-13 00:55:17 +0900

    feat : 대출, 예약 테스트코드 작성

---
+++ src/test/java/com/example/team258/controller/serviceController/BookReservationControllerTest.java
@@ -0,0 +1,109 @@
+package com.example.team258.controller.serviceController;
+
+import com.example.team258.dto.BookDonationEventRequestDto;
+import com.example.team258.dto.BookDonationEventResponseDto;
+import com.example.team258.dto.MessageDto;
+import com.example.team258.entity.BookDonationEvent;
+import com.example.team258.entity.User;
+import com.example.team258.entity.UserRoleEnum;
+import com.example.team258.repository.BookRepository;
+import com.example.team258.security.UserDetailsImpl;
+import com.example.team258.service.AdminDonationEventService;
+import com.example.team258.service.BookRentService;
+import com.example.team258.service.BookReservationService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+class BookReservationControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookReservationService bookReservationService;
+
+    @MockBean
+    private BookRepository bookRepository;
+
+    @Autowired
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    User user1 = User.builder()
+            .userId(1L)
+            .username("user1")
+            .password("1234")
+            .role(UserRoleEnum.USER)
+            .build();
+
+    UserDetails authenticateUser(User user) {
+        UserDetailsImpl userDetails = new UserDetailsImpl(user);
+        Authentication authentication =
+                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return userDetails;
+    }
+
+    @Test
+    void 대출_생성() throws Exception {
+        //given
+        MessageDto msg =  MessageDto.builder()
+                .msg("도서 예약 신청이 완료되었습니다")
+                .build();
+
+        //when
+        when(bookReservationService.createReservation(any(), any(User.class)))
+                .thenReturn(msg);
+
+        //then
+        mockMvc.perform(post("/api/books/{bookId}/reservation", 1L)
+                        .content(objectMapper.writeValueAsString(authenticateUser(user1))))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.msg").value("도서 예약 신청이 완료되었습니다"));
+    }
+
+    @Test
+    void 대출_삭제() throws Exception {
+        //given
+        MessageDto msg =  MessageDto.builder()
+                .msg("도서 예약 취소가 완료되었습니다")
+                .build();
+
+        //when
+        when(bookReservationService.deleteReservation(any(), any(User.class)))
+                .thenReturn(msg);
+
+        //then
+        mockMvc.perform(delete("/api/books/{bookId}/reservation", 1L)
+                        .content(objectMapper.writeValueAsString(authenticateUser(user1))))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.msg").value("도서 예약 취소가 완료되었습니다"));
+    }
+
+}
