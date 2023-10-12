1a445004dab4691ab44b68d521809b51f3696d17
DESKTOP-HMN2L1J\정강용 <tuto3355@naver.com>
2023-10-12 23:21:57 +0900

    feat : 도서 예약 기능 생성

---
+++ src/main/java/com/example/team258/service/BookReservationService.java
@@ -0,0 +1,52 @@
+package com.example.team258.service;
+
+import com.example.team258.dto.MessageDto;
+import com.example.team258.entity.Book;
+import com.example.team258.entity.BookReservation;
+import com.example.team258.entity.BookStatus;
+import com.example.team258.entity.User;
+import com.example.team258.repository.BookRepository;
+import com.example.team258.repository.BookReservationRepository;
+import com.example.team258.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class BookReservationService {
+    private final BookReservationRepository bookReservationRepository;
+    private final BookRepository bookRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public MessageDto createReservation(Long bookId, User user) {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(()->new IllegalArgumentException("book을 찾을 수 없습니다."));
+        User savedUser = userRepository.findById(user.getUserId())
+                .orElseThrow(()->new IllegalArgumentException("user를 찾을 수 없습니다."));
+        if (book.getBookStatus() == BookStatus.POSSIBLE) {
+            throw new IllegalArgumentException("책이 대여 가능한 상태입니다.");
+        }
+
+        BookReservation bookReservation = bookReservationRepository.save(new BookReservation(savedUser, book));
+        book.addBookReservation(bookReservation);
+        savedUser.addBookReservation(bookReservation);//추후 하나의 메소드로 통합하는 방안도 확인하겠음
+
+        return new MessageDto("도서 예약 신청이 완료되었습니다");
+    }
+
+    @Transactional
+    public MessageDto deleteReservation(Long bookId, User user) {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(()->new IllegalArgumentException("book을 찾을 수 없습니다."));
+        User savedUser = userRepository.findById(user.getUserId())
+                .orElseThrow(()->new IllegalArgumentException("user를 찾을 수 없습니다."));
+        BookReservation bookReservation = bookReservationRepository.findByUserAndBook(savedUser, book)
+                .orElseThrow(()->new IllegalArgumentException("대여 기록을 찾을 수 없습니다"));
+
+        bookReservationRepository.deleteById(bookReservation.getBookReservationId());
+
+        return new MessageDto("도서 예약 취소가 완료되었습니다");
+    }
+}
