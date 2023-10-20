a4d627447a7be3bfd0abbfd620a9cb5fe084407f
songyuheon98 <songyuheon2750@gmail.com>
2023-10-20 17:10:37 +0900

    단순 검색 메뉴 프론트에 추가 및 백엔드 서버에서 검색 쿼리 전달 확인

--- src/main/java/com/example/team258/controller/mixedController/AdminMixedController.java
+++ src/main/java/com/example/team258/controller/mixedController/AdminMixedController.java
@@ -40,4 +40,23 @@ public class AdminMixedController {
         return "admin";
     }
 
+    @GetMapping("/admin/users/v2")
+    /**
+     * @RequestParam(defaultValue = "0") int page : page 파라미터가 없으면 0으로 설정
+     */
+    public String adminViewV2(@RequestParam(defaultValue = "0") int page, Model model, @RequestParam(defaultValue = "") String userName,@RequestParam(defaultValue = "") String userRole ) {
+
+        PageRequest pageRequest = PageRequest.of(page, 5);  // page 파라미터로 받은 값을 사용
+        Page<User> users = userRepository.findAll(pageRequest);
+        int totalPages = users.getTotalPages();
+
+        List<UserResponseDto> userResponseDtos = users.stream().map(UserResponseDto::new).collect(Collectors.toList());
+
+        model.addAttribute("currentPage", page);  // 현재 페이지 번호 추가
+        model.addAttribute("totalPages", totalPages);
+        model.addAttribute("users", userResponseDtos);
+
+        return "adminV2";
+    }
+
 }
