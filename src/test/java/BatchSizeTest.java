0bde0e1dc80a2b3870200a04365e854a66ba5616
songyuheon98 <songyuheon2750@gmail.com>
2023-10-24 13:53:59 +0900

    fix

--- src/test/java/BatchSizeTest.java
+++ src/test/java/BatchSizeTest.java
@@ -1,44 +1,44 @@
-import com.example.team258.common.entity.User;
-import com.example.team258.common.repository.UserRepository;
-import com.example.team258.domain.donation.entity.BookApplyDonation;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
-@SpringBootTest
-@AutoConfigureMockMvc(addFilters = false)
-@Transactional
-public class BatchSizeTest {
-
-    private UserRepository userRepository;
-
-    public BatchSizeTest(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    @Test
-    void batchTest(){
-        //given
-        User user = User.builder()
-                .username("test")
-                .password("test")
-                .build();
-
-        BookApplyDonation bookApplyDonation = BookApplyDonation.builder()
-                .book(null)
-                .build();
-
-        userRepository.save(user);
-
-
-        user.addBookApplyDonation(bookApplyDonation);
-
-
-
-
-
-
-    }
-}
+//import com.example.team258.common.entity.User;
+//import com.example.team258.common.repository.UserRepository;
+//import com.example.team258.domain.donation.entity.BookApplyDonation;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@SpringBootTest
+//@AutoConfigureMockMvc(addFilters = false)
+//@Transactional
+//public class BatchSizeTest {
+//
+//    private UserRepository userRepository;
+//
+//    public BatchSizeTest(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+//
+//    @Test
+//    void batchTest(){
+//        //given
+//        User user = User.builder()
+//                .username("test")
+//                .password("test")
+//                .build();
+//
+//        BookApplyDonation bookApplyDonation = BookApplyDonation.builder()
+//                .book(null)
+//                .build();
+//
+//        userRepository.save(user);
+//
+//
+//        user.addBookApplyDonation(bookApplyDonation);
+//
+//
+//
+//
+//
+//
+//    }
+//}
