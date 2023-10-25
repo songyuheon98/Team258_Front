1348f59c078d732a3689036aedc62d43e8a25854
DESKTOP-HMN2L1J\정강용 <tuto3355@naver.com>
2023-10-25 22:42:38 +0900

    refactor : 대출 동시성 오류 해결(비관적 락 사용)

--- src/main/java/com/example/team258/common/repository/BookRepository.java
+++ src/main/java/com/example/team258/common/repository/BookRepository.java
@@ -4,9 +4,11 @@ import com.example.team258.common.entity.Book;
 import com.example.team258.domain.user.entity.BookRent;
 import com.example.team258.common.entity.BookStatusEnum;
 import com.example.team258.common.entity.BookCategory;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
@@ -14,6 +16,10 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 import java.util.Optional;
 public interface BookRepository extends JpaRepository <Book,Long>, QuerydslPredicateExecutor<Book>, BookRepositoryCustom {
+
+    @Lock(LockModeType.PESSIMISTIC_READ)
+    @Query("SELECT b from book b WHERE b.bookId = :bookId")
+    Optional<Book> findByIdLock(@Param("bookId") Long bookId);
 
     Page<Book> findAllByBookNameContaining(String keyword, Pageable pageable);
 
